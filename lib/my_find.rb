1f801f3f1078a2c04410c39c504a1dba17bbb1b6
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 13:01:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,14 +3,10 @@ require 'pry'
 def my_find(collection)
   i = 0
 
-  new_collection = []
-
   while i < collection.length do
     if(yield(collection[i]))
-      new_collection << collection[i]
+      collection[i]
     end
     i = i + 1
   end
-
-  new_collection[0]
 end
