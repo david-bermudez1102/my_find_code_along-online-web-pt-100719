50b9e8ed71643874ddd81dab7136a5aeaf714d2e
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 13:00:25 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   i = 0
 
   while i < collection.length do
-    if(yield(collection[i]) == true)
+    if(yield(collection[i]))
       return collection[i]
     end
     i = i + 1
